9ad9393d4e7302f11a4af8998d08be3e0a9db64d
LM <lmontigny@users.noreply.github.com>
2020-06-28 18:30:19 +0200

    Create cuda_graph.cu

---
+++ cuda_graph.cu
@@ -0,0 +1,16 @@
+bool graphCreated=false;
+cudaGraph_t graph;
+cudaGraphExec_t instance;
+for(int istep=0; istep<NSTEP; istep++){
+  if(!graphCreated){
+    cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+    for(int ikrnl=0; ikrnl<NKERNEL; ikrnl++){
+      shortKernel<<<blocks, threads, 0, stream>>>(out_d, in_d);
+    }
+    cudaStreamEndCapture(stream, &graph);
+    cudaGraphInstantiate(&instance, graph, NULL, NULL, 0);
+    graphCreated=true;
+  }
+  cudaGraphLaunch(instance, stream);
+  cudaStreamSynchronize(stream);
+}
